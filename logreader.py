6cc3b3a19041f21113408f5fcb2fa65c863eec1c
yumimint <82876715+yumimint@users.noreply.github.com>
2021-04-28 10:41:12 +0900

    1秒以内の同一サウンドを発音しないようにした コードの一部をmisc.pyへ移動

--- logreader.py
+++ logreader.py
@@ -1,15 +1,10 @@
 # %%
 
-import urllib.request
-import collections
-import csv
 import ctypes
-import io
 import logging
 import re
 import sys
-import threading
-import time
+import urllib.request
 from pathlib import Path
 
 import bouyomichan
@@ -20,6 +15,9 @@ from colorama import Back, Fore, Style
 
 import chatcmd
 import logpump
+import misc
+
+VERSION = "ver 210428"
 
 colorama.init(autoreset=True)
 
@@ -36,69 +34,9 @@ ChatColors = {
 }
 
 
-class UsersFile:
-    data = {}
-
-    def __init__(self, path, loader):
-        self.mtime = 0
-        self.path = Path(path)
-        self.loader = loader
-
-    def __call__(self):
-        try:
-            t = self.path.stat().st_mtime
-            if self.mtime < t:
-                self.data = self.loader(self.path)
-                self.mtime = t
-        except FileNotFoundError:
-            pass
-        return self.data
-
-
-def spitem_loader(path):
-    """spitem.txtをパースする"""
-    rex = set()
-    dic = {"regexp": rex}
-    current_audio = None
-    with path.open(encoding="utf-8") as f:
-        for line in f.readlines():
-            line = line.strip()
-            if not line or line.startswith("#"):
-                continue
-            if line.startswith("[") and line.endswith("]"):
-                current_audio = line[1:-1]
-                continue
-            if line.startswith("/") and line.endswith("/"):
-                pattern = re.compile(line[1:-1])
-                rex.add((pattern, current_audio))
-            dic[line] = current_audio
-    logging.info(str(path) + " loaded")
-    return dic
-
-
-def read_la_dict(f):
-    dic = {}
-    f.readline()  # skip header line
-    for row in csv.reader(f):
-        if len(row) < 2:
-            continue
-        name, cmd = row[:2]
-        if not cmd:
-            continue
-        dic[cmd] = name
-    return dic
-
-
-def la_dict_loader(path):
-    with path.open(encoding='utf-8') as f:
-        dic = read_la_dict(f)
-    logging.info(str(path) + " loaded")
-    return dic
-
-spitem = UsersFile("spitem.txt", spitem_loader)
-
+spitem = misc.UsersFile("spitem.txt", misc.spitem_loader)
 la_csv = "lobbyactions.csv"
-la_dict = UsersFile(la_csv, la_dict_loader)
+la_dict = misc.UsersFile(la_csv, misc.la_dict_loader)
 
 if not Path(la_csv).is_file():
     try:
@@ -118,38 +56,16 @@ def spitem_check_and_notify(item):
 
     if item in dic:
         sound = dic[item]
-        playsound.playsound(sound, block=False)
+        play_sound(sound)
         return
 
     for pattern, sound in dic["regexp"]:
         if pattern.match(item):
-            playsound.playsound(sound, block=False)
+            play_sound(sound)
 
 
-class TalkativesDetector:
-    """おしゃべり過多検出器"""
-
-    def __init__(self, duration=60):
-        self.od = collections.OrderedDict()
-        self.duration = duration
-
-    def __call__(self, text):
-        """一定期間内に同じtextがあればTrueを返す"""
-        now = time.time()
-        self.forget(now - self.duration)
-        exists = text in self.od
-        self.od[text] = now
-        self.od.move_to_end(text, last=True)
-        return exists
-
-    def forget(self, expiry):
-        for t in list(self.od.values()):
-            if t > expiry:
-                break
-            self.od.popitem(last=False)
-
-
-talkactive = TalkativesDetector()
+talkactive = misc.TalkativesDetector()
+talkactive_sound = misc.TalkativesDetector(1)
 
 
 def talk(text):
@@ -157,172 +73,15 @@ def talk(text):
         bouyomichan.talk(text)
 
 
-class ItemCounter(dict):
-    def add(self, item, num):
-        if item not in self:
-            self[item] = 0
-        self[item] += num
-
-    def names(self):
-        ls = list(filter(lambda x: x[1] > 0, self.items()))
-        ls.sort(key=lambda x: (-x[1], x[0]))
-        return map(lambda x: self.pair2name(*x), ls)
-
-    units = [
-        (lambda item: 'メダル' in item, '枚'),
-        (lambda item: '券' in item, '枚'),
-        (lambda item: '肉' in item, 'Kg'),
-        (lambda item: True, '個'),
-    ]
-
-    @staticmethod
-    def pair2name(item, num):
-        # item = item.translate(ItemCounter.zen2han_alpha)
-        if item == 'メセタ':
-            return f'{num:,}メセタ'
-        if num == 1:
-            return item
-        for _ in ItemCounter.units:
-            if _[0](item):
-                unit = _[1]
-                break
-        return f'{item}({num}{unit})'
-
-    # zen2han_alpha = str.maketrans(
-    #     {chr(0xFF01 + i): chr(0x21 + i) for i in range(93)})
-
-
-class DelayedReporter:
-    delay = 30
-
-    def __init__(self):
-        self.count = ItemCounter()
-        self.added = threading.Event()
-        self.mutex = threading.Lock()
-        self.expiry = 0
-        self.th = threading.Thread(target=self.main, daemon=True)
-        self.keep_running = True
-
-    def start(self):
-        self.th.start()
-
-    def stop(self):
-        self.keep_running = False
-        self.th.join()
-
-    def put(self, item, num):
-        with self.mutex:
-            self.count.add(item, num)
-            self.expiry = time.time() + self.delay
-        self.added.set()
-
-    def main(self):
-        while self.keep_running:
-            if not self.added.wait(timeout=1):
-                continue
-
-            while self.added.is_set():
-                self.added.clear()
-                s = self.expiry - time.time()
-                if s > 0:
-                    time.sleep(s)
-
-            with self.mutex:
-                names = self.count.names()
-                self.count.clear()
-
-            text = ' '.join(names)
-            talk(text)
-            print(Fore.YELLOW + text)
-
-
-class CasinoCounter:
-    """ Amusement
-    """
-
-    printed = False
-
-    def __init__(self):
-        self.reset()
-        self.badcount = [0] * 6
-
-    def reset(self):
-        self.count = 0
-        self.bet = 0
-        self.ret = 0
-        self.hit = 0
-        self.defeats = 0
-        self.defeats_max = 0
-
-    def add(self, bet, ret):
-        self.count += 1
-        self.bet += bet
-        self.ret += ret
-        if ret == 0:
-            self.defeats += 1
-            self.defeats_max = max(self.defeats_max, self.defeats)
-            if self.defeats > 1 and bet <= 5:
-                self.badcount[bet] += 1
-        else:
-            self.defeats = 0
-            self.hit += 1
-
-    @property
-    def rate(self): return self.ret / self.bet
-
-    @property
-    def hitrate(self):
-        return self.hit / self.count
-
-    @property
-    def income(self): return self.ret - self.bet
-
-    @property
-    def meter(self):
-        m, n = self.defeats, self.defeats_max - self.defeats
-        return 'x' * m + '.' * n
-
-    @property
-    def toobad(self):
-        return self.defeats > 0 and self.defeats == self.defeats_max
-
-    def __call__(self, ent):
-        if ent[2] in ['UsePass', 'Buy']:
-            return
-
-        bet, ret, before, after = map(int, ent[5:9])
-
-        self.add(bet, ret)
-
-        c = self
-        if ret:
-            talk(f"{ret}枚当たり 累計{c.income} ヒット率{c.hitrate:.3f}")
-        elif c.defeats > 2:
-            talk(f'{c.defeats}連敗 ')
-
-        print(
-            # ent[0],
-            c.meter,
-            f'{bet}',
-            f'{ret:3d}',
-            f'{c.hit}/{c.count}({c.hitrate:.3f})',
-            f'Total({after:,})',
-            f'Rate({c.rate:.2f}) In({c.income:,})',
-            # *self.badcount,
-            '   ',
-            end='\r')
-
-        CasinoCounter.printed = True
-
-    @classmethod
-    def linefeed(klass):
-        if klass.printed:
-            klass.printed = False
-            print("")
+def play_sound(sound):
+    if not talkactive_sound(sound):
+        playsound.playsound(sound, block=False)
 
 
 def chat_print(ent, text):
     time, seq, channel, id, name = ent[:5]
+    del seq, id
+
     time = time[-8:]
     channel = 'GROUP' if channel == 'CHANNEL' else channel
     try:
@@ -333,8 +92,6 @@ def chat_print(ent, text):
     # name = getattr(Back, col) + Fore.BLACK + name + Style.RESET_ALL
     text = getattr(Fore, col) + text
 
-    CasinoCounter.linefeed()
-
     print(f"{time} {name} {text}")
 
 
@@ -342,6 +99,7 @@ def handle_Chat(ent):
     """ Chat
     """
     time, seq, channel, id, name, mess = ent[:6]
+    del time, seq, id
 
     if channel == 'PARTY' and handle_Amusement.count:
         handle_Amusement.reset()
@@ -373,16 +131,19 @@ def handle_SymbolChat(ent):
     time, seq, channel, id, name, said = ent[:6]
     chat_print(ent, said)
     talk(f'{name}のシンボルアート')
+    del time, seq, channel, id
 
 
-# pickup = ItemCounter()
+def report_handler(text):
+    print(Fore.YELLOW + text)
+    talk(text)
 
-report = DelayedReporter()
+
+report = misc.DelayedReporter(report_handler)
 report.start()
 
 
 def pushitem(item, num):
-    # pickup.add(item, num)
     report.put(item, num)
     spitem_check_and_notify(item)
 
@@ -435,7 +196,7 @@ def handle_Scratch(ent):
         pushitem(item, num)
 
 
-handle_Amusement = CasinoCounter()
+handle_Amusement = misc.CasinoCounter(talk)
 
 
 def on_entry(ent):
@@ -448,7 +209,7 @@ def on_entry(ent):
 
 
 def main():
-    ctypes.windll.kernel32.SetConsoleTitleW("PSO2 Log Reader")
+    ctypes.windll.kernel32.SetConsoleTitleW(f"PSO2LogReader {VERSION}")
     pumpz = logpump.LogPumpz(on_entry)
     pumpz.start()
     print(Fore.GREEN + "START")
@@ -457,9 +218,11 @@ def main():
             line = input().strip()
             if line == "exit":
                 break
+            handle_Amusement.report()
     except KeyboardInterrupt:
         pass
     pumpz.stop()
+    report.stop()
     print("done")
 
 
---
+++ misc.py
@@ -0,0 +1,254 @@
+import collections
+import csv
+import logging
+import pathlib
+import re
+import threading
+import time
+
+
+class UsersFile:
+    data = {}
+
+    def __init__(self, path, loader):
+        self.mtime = 0
+        self.path = pathlib.Path(path)
+        self.loader = loader
+
+    def __call__(self):
+        try:
+            t = self.path.stat().st_mtime
+            if self.mtime < t:
+                self.data = self.loader(self.path)
+                self.mtime = t
+        except FileNotFoundError:
+            pass
+        return self.data
+
+
+def spitem_loader(path):
+    """spitem.txtをパースする"""
+    rex = set()
+    dic = {"regexp": rex}
+    current_audio = None
+    with path.open(encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            if line.startswith("[") and line.endswith("]"):
+                current_audio = line[1:-1]
+                continue
+            if line.startswith("/") and line.endswith("/"):
+                pattern = re.compile(line[1:-1])
+                rex.add((pattern, current_audio))
+                continue
+
+            dic[line] = current_audio
+    logging.info(str(path) + " loaded")
+    return dic
+
+
+def la_dict_loader(path):
+    with path.open(encoding='utf-8') as f:
+        dic = read_la_dict(f)
+    logging.info(str(path) + " loaded")
+    return dic
+
+
+def read_la_dict(f):
+    dic = {}
+    f.readline()  # skip header line
+    for row in csv.reader(f):
+        if len(row) < 2:
+            continue
+        name, cmd = row[:2]
+        if not cmd:
+            continue
+        dic[cmd] = name
+    return dic
+
+
+class TalkativesDetector:
+    """おしゃべり過多検出器"""
+
+    def __init__(self, period=60):
+        self.od = collections.OrderedDict()
+        self.period = period
+
+    def __call__(self, text):
+        """一定期間内に同じtextがあればTrueを返す"""
+        now = time.time()
+        self.forget(now - self.period)
+        exists = text in self.od
+        self.od[text] = now
+        self.od.move_to_end(text, last=True)
+        return exists
+
+    def forget(self, expiry):
+        for t in list(self.od.values()):
+            if t > expiry:
+                break
+            self.od.popitem(last=False)
+
+
+class CasinoCounter:
+    """ Amusement
+    """
+
+    printed = False
+
+    def __init__(self, talker):
+        self.talker = talker
+        self.reset()
+        self.badcount = [0] * 6
+
+    def reset(self):
+        self.count = 0
+        self.bet = 0
+        self.ret = 0
+        self.hit = 0
+        self.defeats = 0
+        self.defeats_max = 0
+
+    def add(self, bet, ret):
+        self.count += 1
+        self.bet += bet
+        self.ret += ret
+        if ret == 0:
+            self.defeats += 1
+            self.defeats_max = max(self.defeats_max, self.defeats)
+            if self.defeats > 1 and bet <= 5:
+                self.badcount[bet] += 1
+        else:
+            self.defeats = 0
+            self.hit += 1
+
+    @property
+    def rate(self):
+        if self.bet == 0:
+            return 0
+        return self.ret / self.bet
+
+    @property
+    def hitrate(self):
+        if self.count == 0:
+            return 0
+        return self.hit / self.count
+
+    @property
+    def income(self): return self.ret - self.bet
+
+    @property
+    def meter(self):
+        m, n = self.defeats, self.defeats_max - self.defeats
+        return 'x' * m + '.' * n
+
+    @property
+    def toobad(self):
+        return self.defeats > 0 and self.defeats == self.defeats_max
+
+    def __call__(self, ent):
+        if ent[2] in ['UsePass', 'Buy']:
+            return
+
+        bet, ret, before, after = map(int, ent[5:9])
+        del before, after
+
+        self.add(bet, ret)
+
+        c = self
+        if ret:
+            self.talker(f"{ret}枚当たり 累計{c.income} ヒット率{c.hitrate:.3f}")
+        elif c.defeats > 2:
+            self.talker(f'{c.defeats}連敗 ')
+
+    def report(self):
+        c = self
+        print(
+            c.meter,
+            # f'{bet}',
+            # f'{ret:3d}',
+            f'{c.hit}/{c.count}({c.hitrate:.3f})',
+            # f'Total({after:,})',
+            f'Rate({c.rate:.2f}) In({c.income:,})',
+        )
+
+
+class ItemCounter(dict):
+    def add(self, item, num):
+        if item not in self:
+            self[item] = 0
+        self[item] += num
+
+    def names(self):
+        ls = list(filter(lambda x: x[1] > 0, self.items()))
+        ls.sort(key=lambda x: (-x[1], x[0]))
+        return map(lambda x: self.pair2name(*x), ls)
+
+    units = [
+        (lambda item: 'メダル' in item, '枚'),
+        (lambda item: '券' in item, '枚'),
+        (lambda item: '肉' in item, 'Kg'),
+        (lambda item: True, '個'),
+    ]
+
+    @staticmethod
+    def pair2name(item, num):
+        # item = item.translate(ItemCounter.zen2han_alpha)
+        if item == 'メセタ':
+            return f'{num:,}メセタ'
+        if num == 1:
+            return item
+        for _ in ItemCounter.units:
+            if _[0](item):
+                unit = _[1]
+                break
+        return f'{item}({num}{unit})'
+
+    # zen2han_alpha = str.maketrans(
+    #     {chr(0xFF01 + i): chr(0x21 + i) for i in range(93)})
+
+
+class DelayedReporter:
+    delay = 30
+
+    def __init__(self, report_cb):
+        self.report_cb = report_cb
+        self.count = ItemCounter()
+        self.added = threading.Event()
+        self.mutex = threading.Lock()
+        self.expiry = 0
+        self.th = threading.Thread(target=self.main, daemon=True)
+        self.keep_running = True
+
+    def start(self):
+        self.th.start()
+
+    def stop(self):
+        self.keep_running = False
+        self.th.join()
+
+    def put(self, item, num):
+        with self.mutex:
+            self.count.add(item, num)
+            self.expiry = time.time() + self.delay
+        self.added.set()
+
+    def main(self):
+        while self.keep_running:
+            if not self.added.wait(timeout=1):
+                continue
+
+            while self.added.is_set():
+                self.added.clear()
+                s = self.expiry - time.time()
+                if s > 0:
+                    time.sleep(s)
+
+            with self.mutex:
+                names = self.count.names()
+                self.count.clear()
+
+            text = ' '.join(names)
+            self.report_cb(text)
