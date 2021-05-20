66523b877f06108bab81924593116638c2bab8aa
yumimint <i.yumimint@gmail.com>
2021-05-20 12:54:11 +0900

    Update playsound.py
    + 音源ファイルは一度だけopenする
    * 再生用スレッドを設けた

--- playsound.py
+++ playsound.py
@@ -1,25 +1,51 @@
+import queue
+import threading
 from ctypes import create_unicode_buffer, windll, wintypes
 from time import time
+
+q = queue.Queue()
+
+
+def playsound(sound, volume=1.0):
+    q.put((sound, volume))
+
+
+aliasz = {}
+
+
+def _daemon():
+    while True:
+        sound, volume = q.get()
+
+        if sound in aliasz:
+            alias = aliasz[sound]
+        else:
+            alias = str(time())
+            try:
+                mciSendString(f'open "{sound}" alias {alias} type mpegvideo')
+            except Exception:
+                continue
+            aliasz[sound] = alias
+
+        mciSendString(f'setaudio {alias} volume to {int(1000 * volume)}')
+        mciSendString(f'seek {alias} to start')
+        mciSendString(f'play {alias}')
+
+
+th = threading.Thread(target=_daemon, daemon=True)
+th.start()
 
 
 def mciSendString(command):
     buf = create_unicode_buffer(wintypes.MAX_PATH)
 
     errorCode = int(
-        windll.winmm.mciSendStringW(command, buf, wintypes.MAX_PATH, 0))
+        windll.winmm.mciSendStringW(command, buf, len(buf), 0))
 
     if errorCode:
-        errorBuffer = create_unicode_buffer(256)
-        windll.winmm.mciGetErrorStringW(errorCode, errorBuffer, 255)
-        raise RuntimeError(
-            f'Error {errorCode}: {errorBuffer.value} for command: {command}'
-        )
+        errorBuffer = create_unicode_buffer(wintypes.MAX_PATH)
+        windll.winmm.mciGetErrorStringW(
+            errorCode, errorBuffer, len(errorBuffer))
+        raise RuntimeError(f'{errorBuffer.value} ({errorCode})')
 
     return buf.value
-
-
-def playsound(sound, volume=1.0):
-    alias = str(time())
-    mciSendString(f'open "{sound}" alias {alias} type mpegvideo')
-    mciSendString(f'setaudio {alias} volume to {int(1000 * volume)}')
-    mciSendString(f'play {alias}')
